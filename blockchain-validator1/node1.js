8593da4d76cc3d2d3e6c7b69eb2158453c3c8aac
saifrehman <s4saif.121@gmail.com>
2018-05-23 00:37:57 +0400

    refactoed

--- blockchain-validator1/node1.js
+++ blockchain-validator1/node1.js
@@ -1,17 +1,32 @@
-require('dotenv').config({path: ".env-node1"});
+require('dotenv').config({ path: ".env-node1" });
+var geodist = require('geodist')
 let lotion = require('lotion')
 let app = lotion({
   genesis: './genesis.json',
   tendermintPort: 30090,
-  initialState: { messages: [] },
-  p2pPort: 30092,
+  initialState: [],
+  p2pPort: 30091,
   logTendermint: true,
   keys: 'privkey0.json',
-  peers: ['localhost:30092']
+  peers: ['localhost:30093']
 })
-app.use((state, tx,chainInfo) => {
-  if (typeof tx.sender === 'string' && typeof tx.message === 'string') {
-    state.messages.push({ sender: tx.sender, message: tx.message })
+app.use((state, tx, chainInfo) => {
+  if (tx[0].gender != tx[1].gender && abs(tx[0].age - tx[1].age) <= 4) {
+    var dist = geodist({ lat: tx[0].userlocation.Lat, lon: tx[0].userlocation.Long }, { lat: tx[1].userlocation.Lat, lon: tx[1].userlocation.Long }, { exact: true, unit: 'km' })
+    if (dist < 1.0) {
+      state.push({
+        userone: {
+          name: tx[0].name,
+          gender: tx[0].gender,
+          age: tx[0].age
+        },
+        usertwo: {
+          name: tx[1].name,
+          gender: tx[1].gender,
+          age: tx[1].age
+        }
+      })
+    }
   }
 })
 app.listen(3000).then(({ GCI }) => {
