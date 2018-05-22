b6ee4d7e9157d8cc36f99cfe361cffa2444042bb
saifrehman <s4saif.121@gmail.com>
2018-05-23 01:05:18 +0400

    added all blockchain logic

--- blockchain-validator1/node1.js
+++ blockchain-validator1/node1.js
@@ -4,17 +4,18 @@ let lotion = require('lotion')
 let app = lotion({
   genesis: './genesis.json',
   tendermintPort: 30090,
-  initialState: [],
+  initialState: { messages: [] },
   p2pPort: 30091,
   logTendermint: true,
   keys: 'privkey0.json',
+  createEmptyBlocks: true,
   peers: ['localhost:30093']
 })
 app.use((state, tx, chainInfo) => {
-  if (tx[0].gender != tx[1].gender && abs(tx[0].age - tx[1].age) <= 4) {
+  if (tx[0].gender != tx[1].gender && Math.abs(tx[0].age - tx[1].age) <= 4) {
     var dist = geodist({ lat: tx[0].userlocation.Lat, lon: tx[0].userlocation.Long }, { lat: tx[1].userlocation.Lat, lon: tx[1].userlocation.Long }, { exact: true, unit: 'km' })
     if (dist < 1.0) {
-      state.push({
+      state.messages.push({
         userone: {
           name: tx[0].name,
           gender: tx[0].gender,
--- blockchain-validator2/node2.js
+++ blockchain-validator2/node2.js
@@ -1,20 +1,22 @@
 require('dotenv').config({path: ".env-node2"});
+var geodist = require('geodist')
 
 let lotion = require('lotion')
 let app = lotion({
   genesis: './genesis.json',
   tendermintPort: 30092,
-  initialState: [],
+  initialState: { messages: [] },
   p2pPort: 30093,
   logTendermint: true,
+  createEmptyBlocks: true,
   keys: 'privkey1.json',
   peers: ['localhost:30091']
 })
 app.use((state, tx, chainInfo) => {
-  if (tx[0].gender != tx[1].gender && abs(tx[0].age - tx[1].age) <= 4) {
+  if (tx[0].gender != tx[1].gender && Math.abs(tx[0].age - tx[1].age) <= 4) {
     var dist = geodist({ lat: tx[0].userlocation.Lat, lon: tx[0].userlocation.Long }, { lat: tx[1].userlocation.Lat, lon: tx[1].userlocation.Long }, { exact: true, unit: 'km' })
     if (dist < 1.0) {
-      state.push({
+      state.messages.push({
         userone: {
           name: tx[0].name,
           gender: tx[0].gender,
