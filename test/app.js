72efcc7abd0e045da4d8730df172cb35086058a9
saifrehman <s4saif.121@gmail.com>
2018-05-22 22:40:39 +0400

    removed non valid files

--- test/app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const os = require('os');
-var handler = function(request, response) {
-  console.log("Received request from " + request.connection.remoteAddress);
-  response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
-};
-var www = http.createServer(handler);
-www.listen(8888);
