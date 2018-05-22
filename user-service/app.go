8eff6e24d390d2d0dbd85afd53a662c39413652b
saifrehman <s4saif.121@gmail.com>
2018-05-22 18:25:44 +0400

    added all apis

--- user-service/app.go
+++ user-service/app.go
@@ -37,17 +37,28 @@ func GetAllUserEndpoint(w http.ResponseWriter, req *http.Request){
 }
 
 func CreateUserEndpoint(w http.ResponseWriter, req *http.Request){
-
+	params:=mux.Vars(req)
+	var user User
+	_ = json.NewDecoder(req.Body).Decode(&user)
+	users = append(users,user)
+	json.NewEncoder(w).Encode(users)
 }
 
 func DeleteUserEndpoint(w http.ResponseWriter, req *http.Request){
-
+	params:=mux.Vars(req)
+	for index,item  := range people {
+		if item.ID == params["id"]{
+			users = append(users[:index],users[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(users)
 }
 func main(){
 	 router:=mux.NewRouter()
 	 router.HandleFunc("/allUser",GetUserEndpoint).Methods("GET")
 	 router.HandleFunc("/user/{id}",GetAllUserEndpoint).Methods("GET")
-	 router.HandleFunc("/createUser/{id}",CreateUserEndpoint).Methods("POST")
+	 router.HandleFunc("/createUser",CreateUserEndpoint).Methods("POST")
 	 router.HandleFunc("/deleteUser/{id}",DeleteUserEndpoint).Methods("DELETE")
 	 log.Fatal(http.ListenAndServe(:8080,router))
 }
